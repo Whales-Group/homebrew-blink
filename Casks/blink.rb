b129856bda5ee48ab60c3b659932abbe758b4c9a
Finn Jesse Dan <83787516+Jesse-Dan@users.noreply.github.com>
2026-07-05 03:42:24 +0100

    Update download URLs for ARM and Intel versions in blink.rb

--- Casks/blink.rb
+++ Casks/blink.rb
@@ -2,11 +2,11 @@ cask "blink" do
   version "0.1.0"
 
   on_arm do
-    url "https://apps-verity-drive.ckugq3.easypanel.host/blink/uploads/blink/712104b1-57d8-4a61-9d31-708795130339-blink-aarch64.dmg"
+    url "https://apps-verity-drive.ckugq3.easypanel.host/blink/uploads/blink/ae609b4a-72ac-48d7-9936-34bbedcfe408-blink-aarch64.dmg"
     sha256 "76f8f45bcfefca93d8337599926f26e40c93793004be8e261d4f595e10ae458c"
   end
   on_intel do
-    url "https://apps-verity-drive.ckugq3.easypanel.host/blink/uploads/blink/14327468-23a6-45b7-bca8-61924c9d56ea-blink-x64.dmg"
+    url "https://apps-verity-drive.ckugq3.easypanel.host/blink/uploads/blink/3029a4b0-9ec8-4424-a1d4-5698166f6477-blink-x64.dmg"
     sha256 "09194b1930a190b94d842b133ade0b62df6df16fc37842a60e5c59f0022b2c46"
   end
 
