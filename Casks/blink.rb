1844dc37c1adb35233939a6f37d5c09c1a8383c3
Finn Jesse Dan <83787516+Jesse-Dan@users.noreply.github.com>
2026-07-13 02:45:38 +0100

    Update download URLs and SHA-256 hashes for ARM and Intel versions in blink.rb

--- Casks/blink.rb
+++ Casks/blink.rb
@@ -2,12 +2,12 @@ cask "blink" do
   version "0.1.0"
 
   on_arm do
-    url "https://apps-verity-drive.ckugq3.easypanel.host/blink/uploads/blink/59ec2334-69a6-403f-89ad-d7d513f8a4d1-blink-aarch64.dmg.zip"
-    sha256 "819266710cb595d2f988383298099460b6bc39ed131ff3aaebef153a48d7d0b7"
+    url "https://apps-verity-drive.ckugq3.easypanel.host/blink/uploads/blink/e434ed2b-9306-4734-966c-62a7f3006c2a-blink-aarch64.dmg"
+    sha256 "db3749db4ebe9948710b9839b373b0ae5db9e939ece1a58c4a7b4c1dfff85b9f"
   end
   on_intel do
-    url "https://apps-verity-drive.ckugq3.easypanel.host/blink/uploads/blink/52c160de-c562-4098-be1e-45c3b06e3bbf-blink-setup.exe"
-    sha256 "aee00f16bdb1bbbd9c97a783be1c97eaa21dc32458a88d71e34ce8c10fffea6a"
+    url "https://blink.digitwhale.com/downloads/blink-x64.dmg"
+    sha256 "b2007d8376432081eebd10ea051e4b917cb9b8be09c2e10be509fc499015fda3"
   end
 
   name "Blink Desktop"
